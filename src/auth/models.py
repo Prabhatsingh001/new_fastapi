14140f46fe0f1b7fd7b6eef8362cc772b3d4dfdb
Prabhat <87269080+Prabhatsingh001@users.noreply.github.com>
2025-03-10 20:38:40 +0530

    ok

--- src/auth/models.py
+++ src/auth/models.py
@@ -29,3 +29,17 @@ class User(SQLModel, table=True):
         )
     )
     username: str
+    email: str
+    password: str
+    first_name: str
+    last_name: str
+    is_verified: bool = Field(default=False)
+    created_at: datetime = Field(sa_column=Column(pq.TIMESTAMP, default=datetime.now))
+    updated_at: datetime = Field(sa_column=Column(pq.TIMESTAMP, default=datetime.now))
+    # last_login: datetime
+    is_active: bool = Field(default=True)
+    # is_superuser: bool
+
+
+    def __repr__(self):
+        return f"<User {self.username}>"
